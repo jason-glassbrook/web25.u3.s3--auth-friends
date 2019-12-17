d7fc730de519f2a87cedb662c15e564d62c52357
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-17 17:53:09 -0500

    add index to routes

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-import client from 'routes/client';
+import { client } from 'routes';
 
 const PrivateRoute = ({ component : Component, ...rest }) => {
   return (
