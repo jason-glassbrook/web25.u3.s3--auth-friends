4b810c5a0128c243cd0404197e5358413a3489a4
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-17 19:05:01 -0500

    fill out App

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,55 @@
 /// external modules ///
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Switch,
+} from 'react-router-dom';
 
-/// styles ///
-import './styles/App.css';
+/// components ///
+import PrivateRoute from 'components/PrivateRoute';
+import LogIn from 'components/LogIn';
+import LogOut from 'components/LogOut';
+import Friends from 'components/Friends';
 
 /***************************************
   COMPONENT
 ***************************************/
 const App = () => {
   return (
-    <div className='App'>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <nav><ul><li>
+            <NavLink to="/friends">Friends</NavLink>
+          </li><li>
+            <NavLink to="/login">Log In</NavLink>
+          </li><li>
+            <NavLink to="/logout">Log Out</NavLink>
+          </li></ul></nav>
+        </header>
+        <main>
+          <Switch>
+            <PrivateRoute
+            path="/friends"
+            component={Friends}
+            />
+            <Route
+            path="/login"
+            component={LogIn}
+            />
+            <Route
+            path="/logout"
+            component={LogOut}
+            />
+            <Route
+            component={LogIn}
+            />
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
 }
 
