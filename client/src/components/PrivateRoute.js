17f2d629d0988394f4ae12c38c6353e6bc70c624
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-17 19:03:34 -0500

    use funs from /tools/user

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 import { client } from 'routes';
+import user from 'tools/user'
 
 const PrivateRoute = ({ component : Component, ...rest }) => {
   return (
     <Route {...rest} render={(props) => {
-      if (window.localStorage.getItem ('token')) {
+      if (user.token.get () && user.isAllowed.get ()) {
         return (<Component {...props}/>);
       }
       else {
